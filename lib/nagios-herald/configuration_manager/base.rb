3d8dfae8543e54f8efa8fc0a9ec11fcd56a16b80
Nassim Kammah <nkammah@etsy.com>
2014-02-13 10:48:42 -0500

    handle config file errors

--- lib/nagios-herald/configuration_manager/base.rb
+++ lib/nagios-herald/configuration_manager/base.rb
@@ -49,7 +49,7 @@ module NagiosHerald
       def get(section = nil, key = nil)
         return @config if section.nil?
         return @config[section] if key.nil?
-        return @config[section][key]
+        return @config.fetch(section,{})[key]
       end
     end
   end
