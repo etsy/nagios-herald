b14692e9e57dc10ec402be498b903fd4758e52cf
cha <cha@etsy.com>
2014-02-24 17:38:14 +0000

    turn AppConf to a hash to use fetch

--- lib/nagios-herald/configuration_manager/base.rb
+++ lib/nagios-herald/configuration_manager/base.rb
@@ -49,7 +49,7 @@ module NagiosHerald
       def get(section = nil, key = nil)
         return @config if section.nil?
         return @config[section] if key.nil?
-        return @config.fetch(section,{})[key]
+        return @config.to_hash.fetch(section, {})[key]
       end
     end
   end
