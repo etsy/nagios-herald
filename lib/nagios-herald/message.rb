7cbb0fd01f14dc8b53c4908fb5cce6e292863788
Ryan Frantz <rfrantz@etsy.com>
2014-04-03 16:04:43 +0000

    Prep work for the new Message-type objects.

--- lib/nagios-herald/message.rb
+++ lib/nagios-herald/message.rb
@@ -1,3 +1,3 @@
 require 'nagios-herald/message/email_message'
-#require 'nagios-herald/message/pager_message'
-#require 'nagios-herald/message/irc_message'
+require 'nagios-herald/message/pager_message'
+require 'nagios-herald/message/irc_message'
---
+++ lib/nagios-herald/message/pager_message.rb
@@ -0,0 +1,1 @@
+# future home of the PagerMessage class
