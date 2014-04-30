d3d0a27bac59f1f6b4ae336c611374f69b1f820f
Ryan Frantz <rfrantz@etsy.com>
2014-04-30 13:52:39 +0000

    We no longer need #constantize.

--- lib/nagios-herald/util.rb
+++ lib/nagios-herald/util.rb
@@ -34,35 +34,5 @@ module NagiosHerald
       return words.map! {|w| w.capitalize}.join('')
     end
 
-    # TODO: replace constantize with code that loads classes automatically
-    # tries to find a constant with the name specified in the argument string
-    # (from Ruby on Rails?)
-    # this is used to return an object of the specified name?
-    def self.constantize(camel_cased_word)
-      names = camel_cased_word.split('::')
-      names.shift if names.empty? || names.first.empty?
-
-      names.inject(Object) do |constant, name|
-        if constant == Object
-          constant.const_get(name)
-        else
-          candidate = constant.const_get(name)
-          next candidate if constant.const_defined?(name, false)
-          next candidate unless Object.const_defined?(name)
-
-          # Go down the ancestors to check it it's owned
-          # directly before we reach Object or the end of ancestors.
-          constant = constant.ancestors.inject do |const, ancestor|
-            break const    if ancestor == Object
-            break ancestor if ancestor.const_defined?(name, false)
-            const
-          end
-
-          # owner is in Object, so raise
-          constant.const_get(name, false)
-        end
-      end
-    end
-
   end
 end
