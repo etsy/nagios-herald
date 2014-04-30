69e93e160fd7d2e26dd65444126719058eeaa072
Ryan Frantz <rfrantz@etsy.com>
2014-04-30 13:46:58 +0000

    Documenting formatter_loader.rb.

--- lib/nagios-herald/formatter_loader.rb
+++ lib/nagios-herald/formatter_loader.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 
-# NOTES
-# ClassLoader needs a 'self.inherited' method that creates a hash where the
-# keys are the snake_cased names of the classes and the values are the actual classes
-# ala https://github.com/opscode/chef/blob/11-stable/lib/chef/knife.rb#L79#L83
-
+# This is why open source rocks my worl'! I lifted this pattern of loading classes
+# from the good folks at Chef. The gist is that a given directory contains a set
+# of class files that we want to load into the namespace for use later.
+# This allows us to build up a hash of the classes where the key is the class file
+# name and the value is the class itself. We can then instantiate an object of a
+# given class dynamically.
+# See https://github.com/opscode/chef/blob/11-stable/lib/chef/knife.rb#L79#L83 for
+# an example of this in action via the #inherited method.
 module NagiosHerald
   class FormatterLoader
 
@@ -15,24 +18,25 @@ module NagiosHerald
       @formatter_path = File.expand_path("formatters", File.dirname(__FILE__))
     end
 
-    # return an array of formatter class files' absolute paths
+    # Public: Enumerate the available formatter class files.
+    #
+    # Returns an array of the class files' absolute paths
     def enum_formatter_class_files(formatter_path)
       formatter_class_files = Dir.glob(File.expand_path("*.rb", formatter_path))
     end
 
-    # return an array of all class files we care about
+    # Public: Return an array of class files' paths.
     def formatter_class_files
       @formatter_class_files ||= enum_formatter_class_files(@formatter_path)
     end
 
-    # load the formatters
-    # bail if we don't find any formatters
+    # Public: Load the formatters into the namespace.
+    # A formatter can then easily be instantiated later.
     def load_formatters
       if formatter_class_files.empty?
         puts "#{$0}: No formatters were found in '#{@formatter_path}'"
         exit 1
       else
-        #puts "#{formatter_class_files}"    # debug
         formatter_class_files.each do |formatter_class_file|
           Kernel.load formatter_class_file
         end
