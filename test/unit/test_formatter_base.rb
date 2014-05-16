eb0fb895128d3445279d98d3f05002b476f35eec
Ryan Frantz <rfrantz@etsy.com>
2014-05-16 02:18:44 +0000

    Created shells for upcoming test methods.
    - So much work to do but I'm headed to bed.
    - Related to #4.

--- test/unit/test_formatter_base.rb
+++ test/unit/test_formatter_base.rb
@@ -44,26 +44,82 @@ class TestFormatterBase < MiniTest::Unit::TestCase
     assert_equal "Action URL: http://runbook.example.com/disk_space_alerts.html\n\n", @formatter.content[:text][:action_url]
     assert_equal "<b>Action URL</b>: http://runbook.example.com/disk_space_alerts.html<br><br>", @formatter.content[:html][:action_url]
   end
-# more tests to be written...
-#    def host_info
-#    def state_info
-#    def notification_info
-#    def additional_info
-#    def additional_details
-#    def notes
-#    def action_url
-#    def short_state_detail
-#    def recipients_email_link
-#    def ack_info
-#    def short_ack_info
-#    def alert_ack_url
-#    def generate_section(section, *section_style_args)
-#    def generate_problem_content
-#    def generate_recovery_content
-#    def generate_ack_content
-#    def generate_content(nagios_notification_type)
-#    def generate_message_content
-#    def get_sandbox_path
-#    def clean_sandbox
+
+  def test_host_info
+    @formatter.host_info
+  end
+
+  def test_state_info
+    @formatter.state_info
+  end
+
+  def test_notification_info
+    @formatter.notification_info
+  end
+
+  def test_additional_info
+    @formatter.additional_info
+  end
+
+  def test_additional_details
+    @formatter.additional_details
+  end
+
+  def test_notes
+    @formatter.notes
+  end
+
+  def test_action_url
+    @formatter.action_url
+  end
+
+  def test_short_state_detail
+    @formatter.short_state_detail
+  end
+
+  def test_recipients_email_link
+    @formatter.recipients_email_link
+  end
+
+  def test_ack_info
+    @formatter.ack_info
+  end
+
+  def test_short_ack_info
+    @formatter.short_ack_info
+  end
+
+  def test_alert_ack_url
+    @formatter.alert_ack_url
+  end
+
+  def test_generate_problem_content
+    @formatter.generate_problem_content
+  end
+
+  def test_generate_recovery_content
+    @formatter.generate_recovery_content
+  end
+
+  def test_generate_ack_content
+    @formatter.generate_ack_content
+  end
+
+  def test_generate_content
+    @formatter.generate_content(nagios_notification_type)
+  end
+
+  def test_generate_message_content
+    @formatter.generate_message_content
+  end
+
+  def test_get_sandbox_path
+    @formatter.get_sandbox_path
+  end
+
+  def test_clean_sandbox
+    @formatter.clean_sandbox
+  end
 
 end
+
